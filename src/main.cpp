59a56baa0101eb7a7b3aa126b1b4a152c8a57a26
AntoineGaehwiler <antoine.gahwiler@gmail.com>
2023-10-18 19:42:05 +0200

    DebugMode and Transceiver

--- src/main.cpp
+++ src/main.cpp
@@ -12,41 +12,79 @@
 #include <esp_now.h>
 #include <WiFi.h>
 #include <Adafruit_NeoPixel.h>
+#include "AiEsp32RotaryEncoder.h"
 
 // Defines for LED
 #define PIN 25
 #define PIXELCOUNT 8
+
+#define ENCODER_CLK 26
+#define ENCODER_DT 27
+#define ENCODER_SW 14
+#define ROTARY_ENCODER_STEPS 4
+
 const int led = 2;
 Adafruit_NeoPixel strip = Adafruit_NeoPixel(PIXELCOUNT, PIN, NEO_RGB + NEO_KHZ800);
+AiEsp32RotaryEncoder rotaryEncoder = AiEsp32RotaryEncoder(ENCODER_DT, ENCODER_CLK, ENCODER_SW, -1, ROTARY_ENCODER_STEPS);
 
 bool occupied;
+bool rotaryPressed = false;
+bool debugMode = false;
 
-// Structure example to receive data
-// Must match the sender structure
-typedef struct struct_message {
-    int a;
-    bool b;
-} struct_message;
+uint8_t receiverAddress[] = {0x30, 0xAE, 0xA4, 0x7B, 0x79, 0x90};
+esp_now_peer_info_t peerInfo;
 
-// Create a struct_message called myData
-struct_message myData;
+// Data for ESP-Now connection
+typedef struct receivedMessage {
+  bool statusWC;
+  int distanzWC;
+} receivedMessage;
 
-// callback function that will be executed when data is received
-void OnDataRecv(const uint8_t * mac, const uint8_t *incomingData, int len) {
-  memcpy(&myData, incomingData, sizeof(myData));
+typedef struct messageToBeSent {
+  bool changeDistance;
+  int setDistance;
+} messageToBeSent;
 
-  Serial.print("Bool: ");
-  Serial.println(myData.b);
-  occupied = myData.b;
+messageToBeSent myMessageToBeSent;
+receivedMessage myReceivedMessage;
 
-  if (myData.b)
+void readValueOfRotary(){
+  if(debugMode){Serial.println("Rotary Value");Serial.println(rotaryEncoder.readEncoder());}
+  myMessageToBeSent.setDistance = rotaryEncoder.readEncoder();
+  myMessageToBeSent.changeDistance = true;
+  digitalWrite(led, HIGH);
+
+  delay(100);
+}
+
+void messageReceived(const uint8_t* macAddr, const uint8_t* incomingData, int len){
+memcpy(&myReceivedMessage, incomingData, sizeof(myReceivedMessage));
+
+  occupied = myReceivedMessage.statusWC;
+  if(debugMode){
+    Serial.println("Status von ESP32");
+    Serial.println(myReceivedMessage.statusWC);
+    Serial.println("Distanz gemessen");
+    Serial.println(myReceivedMessage.distanzWC);
+    }
+
+  if (myReceivedMessage.statusWC)
   {
     digitalWrite(led, HIGH);
   }else
   {
     digitalWrite(led, LOW);
   }
-  Serial.println();
+
+  esp_err_t result = esp_now_send(receiverAddress, (uint8_t *) &myMessageToBeSent, sizeof(myMessageToBeSent));
+    if (result != ESP_OK) {
+        Serial.println("Sending error");
+    }
+}
+
+void IRAM_ATTR readEncoderISR()
+{
+    rotaryEncoder.readEncoder_ISR();
 }
  
 void setup() {
@@ -55,7 +93,16 @@ void setup() {
 
   // Set Pin to Pin 2
   pinMode(led, OUTPUT);
-  
+
+  rotaryEncoder.begin();
+  rotaryEncoder.setup(readEncoderISR);
+
+  bool circleValues = false;
+
+  rotaryEncoder.setBoundaries(0, 1000, circleValues);
+  rotaryEncoder.disableAcceleration();
+
+
   // Set device as a Wi-Fi Station
   WiFi.mode(WIFI_STA);
 
@@ -65,19 +112,45 @@ void setup() {
     return;
   }
   
-  // Once ESPNow is successfully Init, we will register for recv CB to
-  // get recv packer info
-  esp_now_register_recv_cb(OnDataRecv);
+  esp_now_register_recv_cb(messageReceived);
+    
+  memcpy(peerInfo.peer_addr, receiverAddress, 6);
+  peerInfo.channel = 0;
+  peerInfo.encrypt = false;
+
+  if (esp_now_add_peer(&peerInfo) != ESP_OK) {
+    Serial.println("Failed to add peer");
+    return;
+  }
 
   strip.begin();
   strip.setBrightness(100);
   strip.show();
-
-
 }
  
 void loop() {
-    if (myData.b) 
+
+  if(rotaryEncoder.isEncoderButtonClicked() && rotaryPressed == true){
+   rotaryPressed = false;
+   myMessageToBeSent.changeDistance = false;
+   Serial.println("Modus Ausschalten");
+   digitalWrite(led, LOW);
+   delay(1000);
+  }
+
+  if(rotaryEncoder.isEncoderButtonClicked() && rotaryPressed == false){
+    rotaryPressed = true;
+    delay(1000);
+    Serial.println("Distanz einstellen Mode");
+  }
+
+  if(rotaryPressed){
+    readValueOfRotary();
+    Serial.println(myMessageToBeSent.setDistance);
+  }
+  delay(100);
+
+  if (occupied) 
   {
     int count = 0;
     do{
@@ -92,5 +165,16 @@ void loop() {
       strip.show();
       count++;
     }while(count <= PIXELCOUNT);
+  }
+
+  if(debugMode){
+    Serial.println("StatusWC");
+    Serial.println(myReceivedMessage.statusWC);
+    Serial.println("DistanzWC");
+    Serial.println(myReceivedMessage.distanzWC);
+    Serial.println("changeDistance");
+    Serial.println(myMessageToBeSent.changeDistance);
+    Serial.println("rotaryPressed");
+    Serial.println(rotaryEncoder.isEncoderButtonClicked());
   }
 }
